3e7c92d15962310fe04129175936de70ca759720
chayacohen <chayac100@gmail.com>
2021-12-07 21:45:26 -0500

    added semi-functioning modals, need function to modal buttons

--- src/scripts/game_view.js
+++ src/scripts/game_view.js
@@ -12,30 +12,53 @@ class GameView {
     fillScreen() {
         this.canvas.width = window.innerWidth; 
         this.canvas.height = window.innerHeight * .78;
-    } 
+    };
 
     eventListeners() {
         const instructionsButton = document.querySelector("#instructions")
         const modal = document.querySelector(".modal");
+        const welcomeModal = document.querySelector(".welcome-modal")
+        const pauseModal = document.querySelector(".pause-modal")
+        const gameOverModal = document.querySelector(".game-over-modal")
         const closeButton = document.querySelector(".close-button");
+        const welcomeCloseButton = document.querySelector(".welcome-close-button")
+        const pauseCloseButton = document.querySelector(".pause-close-button")
+        const gameCloseButton = document.querySelector(".game-close-button")
 
         document.addEventListener("click", (event) => {
             if (event.target === instructionsButton)
-            modal.style.display = "block"
-        })
+            modal.style.display = "block";
+        });
 
         document.addEventListener("click", (event) => {
             if(event.target === closeButton) {
-                modal.style.display = "none"
-            }
-        })
+                modal.style.display = "none"};
+
+            if (event.target === welcomeCloseButton) {
+                welcomeModal.style.display = "none"
+            };
+            if (event.target === pauseCloseButton) {
+                pauseModal.style.display = "none"
+            };
+            if (event.target === gameCloseButton) {
+                gameOverModal.style.display = "none"
+            };
+        });
 
         document.addEventListener("click", (event) => {
             if (event.target === modal) {
-                modal.style.display = "none"
-            }
-        })
-    }
+                modal.style.display = "none" };
+            if (event.target === welcomeModal) {
+                welcomeModal.style.display = "none"
+            };
+            if (event.target === pauseModal) {
+                pauseModal.style.display = "none"
+            };
+            if (event.target === gameOverModal) {
+                gameOverModal.style.display = "none"
+            };
+        });
+    };
 }
 
 export default GameView;  
